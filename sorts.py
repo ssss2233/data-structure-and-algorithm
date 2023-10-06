44b74e761c817472b0434569e4bef4a70905a83b
tingfeng <1021849151@qq.com>
2023-10-06 10:41:34 +0800

    sort

--- sorts.py
+++ sorts.py
@@ -15,6 +15,66 @@ class BubbleSort():
                     isSwap = True
             if isSwap == False:
                 break
+class InsertSort():
+    def insert_sort(self,nums):
+        n = len(nums)
+        for i in range(1,n):
+            base = nums[i]
+            insert_point = 0   #插入点位置
+            for j in range(i-1,-2,-1):
+                insert_point = j+1
+                if nums[j] > base and j >= 0:
+                    nums[j+1] = nums[j]
+                else:
+                    break              
+            nums[insert_point] = base
+class QuickSort():
+    def partion(self,nums,left,right):
+        i, j = left+1, right
+        while(i<j):
+            while(i<j and nums[j]>nums[left]): j-=1
+            while(i<j and nums[i]<nums[left]): i+=1
+            if(i<j):
+                nums[i],nums[j] = nums[j], nums[i]
+        nums[left],nums[i] = nums[i],nums[left]   
+        return i  
+    def quick_sort(self,nums,left,right):
+        if(left >= right):
+            return
+        pivot = self.partion(nums,left,right)
+        self.quick_sort(nums,left,pivot-1)
+        self.quick_sort(nums,pivot+1,right)
+
+class MergeSort():
+    def merge(self,nums,left,mid,right):
+        tmp = list(nums[left:right+1])  # 索引是[0:right-left]
+        left_start = 0
+        left_end = mid - left
+        right_start = mid+1-left
+        right_end = right - left
+        i = left_start
+        j = right_start
+        for k in range(left,right+1):
+            if i > left_end:
+                nums[k] = tmp[j]
+                j+=1
+            elif j > right_end or tmp[j]>=tmp[i]:
+                nums[k] = tmp[i]
+                i+=1
+            else:
+                nums[k] = tmp[j]
+                j += 1
+    def merge_sort(self,nums,left,right):
+        if(left >= right):
+            return
+        mid = (left+right)//2
+        self.merge_sort(nums,left,mid)
+        
+        self.merge_sort(nums,mid+1,right)
+        
+        self.merge(nums,left,mid,right)
+        
+        
 
 
 def main():
@@ -30,4 +90,23 @@ def main():
     bubble_obj.bubble_sort(nums)
 
     print("-------冒泡排序：",nums)
+
+
+    nums = [7,1,5,3,2,4]
+    insert_obj = InsertSort()
+    insert_obj.insert_sort(nums)
+
+    print("-------插入排序：",nums)
+
+    nums = [7,1,5,3,2,4]
+    quick_obj = QuickSort()
+    quick_obj.quick_sort(nums,0,len(nums)-1)
+
+    print("-------快速排序：",nums)
+
+    nums = [7,1,5,3,2,4]
+    merge_obj = MergeSort()
+    merge_obj.merge_sort(nums,0,len(nums)-1)
+
+    print("-------归并排序：",nums)
 main()
